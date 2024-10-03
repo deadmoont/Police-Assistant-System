a47d0eb6978dda9529612163bfbca6bf84ff778f
Grish Gautam <grishgautam03@gmail.com>
2024-10-03 11:35:48 +0530

    hi

--- Client/src/components/Footer.jsx
+++ Client/src/components/Footer.jsx
@@ -3,7 +3,7 @@ export default function Footer() {
     
     return (
         <footer>
-        <p>&copy; 2024 Police Department Assistance System. All Rights Reserved.</p>
+        <p>&copy; 2024 Police Department Assistance System. All Rights Reserved.Done by Deadmoont</p>
     </footer>
     )
 }

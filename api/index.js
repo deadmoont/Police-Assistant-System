57c3ebeba39dd15cacced6a6f6d6c400cf29285e
Grish Gautam <grishgautam03@gmail.com>
2025-06-23 22:33:35 +0530

    change

--- api/index.js
+++ api/index.js
@@ -22,7 +22,7 @@ const PORT = process.env.PORT || 3001; // Use environment variable for the port 
 app.use(express.json());
 app.use(
   cors({
-    origin: "http://localhost:5173", // Front-end origin;
+    origin: process.env.CLIENT_URL, // Front-end origin;
     credentials: true,
   })
 );
